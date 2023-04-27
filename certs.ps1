f31d788bc9006f18db39d2835f1eedcd84dba57f
mauaksu <38956087+mauaksu@users.noreply.github.com>
2023-04-27 11:00:48 +0200

    Create certs.ps1
    inintial

---
+++ certs.ps1
@@ -0,0 +1,17 @@
+cd C:\temp\certs
+
+[array]$certs = $null
+
+$crtfiles = Get-Item *.crt
+
+foreach ($file in $crtfiles) {
+    $certObj = New-Object -TypeName System.Security.Cryptography.X509Certificates.X509Certificate2($file)
+    $certs += $certObj
+}
+
+$CertStore = New-Object System.Security.Cryptography.X509Certificates.X509Store -ArgumentList "\\$($env:COMPUTERNAME)\My", "LocalMachine"
+
+$CertStore.Open('ReadWrite')
+
+foreach ($cert in $certs){$CertStore.Add($cert)}
+$CertStore.Close()
